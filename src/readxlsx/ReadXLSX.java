7d666e33dad1b8a2e14becb6f5877e55559c1b75
wheslley <wheslleyny@gmail.com>
2018-10-05 18:26:31 -0300

    v01

---
+++ src/readxlsx/ReadXLSX.java
@@ -0,0 +1,96 @@
+package readxlsx;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class ReadXLSX {
+    
+    public static Integer line = 2;
+    public static String path = "D:\\Cutrale\\0.Wheslley\\Archives\\2Real_safra\\";
+    public static String archiveRead = "2Real_safra.xls";
+    public static String subNameArchive = "REAL_SAFRA";
+    
+    public static void main(String[] args) throws IOException, BiffException {
+
+        File arquivo = new File(path + archiveRead);
+
+        Workbook workbook = Workbook.getWorkbook(arquivo);
+
+        Sheet sheet = workbook.getSheet(0);
+        int linhas = sheet.getRows();
+                
+        String nameArchive = null;
+        String nameArchiveOld = null;
+        String key = null;
+        String data = "";
+
+        for (int i = 1; i < linhas; i++) {
+
+            if (key == null) {
+                key = sheet.getCell(0, i).getContents();
+                nameArchive = getNameArchive(sheet, i);
+                nameArchiveOld = nameArchive;
+            } else {
+                nameArchive = getNameArchive(sheet, i);
+            }
+                        
+            if (!sheet.getCell(0, i).getContents().equalsIgnoreCase(key)) {
+                generateTxt(nameArchiveOld, subNameArchive, data);
+                key = null;
+                data = "";
+            }
+
+            data += lineXlx(sheet, i);
+        }
+
+        workbook.close();
+
+    }
+
+    public static String getNameArchive(Sheet sheet, Integer i) {
+        if (sheet.getCell(1, i).getContents().length() > 20) {
+            return sheet.getCell(0, i).getContents() + "_" + sheet.getCell(1, i).getContents().substring(0, 20);
+        } else {
+            return sheet.getCell(0, i).getContents() + "_" + sheet.getCell(1, i).getContents();
+        }
+    }
+
+    public static String lineXlx(Sheet sheet, Integer i) {
+        
+        System.out.println("Linha: " + line);
+        line++;
+        
+        return sheet.getCell(0, i).getContents().replaceAll("%", "") + ";"
+                + sheet.getCell(1, i).getContents().replaceAll("%", "") + ";"
+                + sheet.getCell(2, i).getContents().replaceAll("%", "") + ";"
+                + sheet.getCell(3, i).getContents().replaceAll("%", "") + ";"
+                + sheet.getCell(4, i).getContents().replaceAll("%", "") + ";"
+                + sheet.getCell(5, i).getContents().replaceAll("%", "") + ";"
+                + sheet.getCell(6, i).getContents().replaceAll("%", "") + ";"
+                + sheet.getCell(7, i).getContents().replaceAll("%", "") + ";"
+                + sheet.getCell(8, i).getContents().replaceAll("%", "") + ";"
+                + sheet.getCell(9, i).getContents().replaceAll("%", "") + ";"
+                + sheet.getCell(10, i).getContents().replaceAll("%", "") + ";"
+                + sheet.getCell(11, i).getContents().replaceAll("%", "") + ";"
+                + sheet.getCell(12, i).getContents().replaceAll("%", "") + ";\r\n";
+
+    }
+
+    public static void generateTxt(String nameArchive, String typeArchive, String data) throws IOException {
+
+        FileWriter arq = new FileWriter(path + typeArchive + "_" + nameArchive + ".txt");
+
+        PrintWriter gravarArq = new PrintWriter(arq);
+
+        gravarArq.printf(data);
+
+        arq.close();
+
+    }
+
+}
